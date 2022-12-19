cdfd8f0c0765aaf6edd3f6f6cb48e5aea57adf4f
Anna_Osn <Anya_kas@live.ru>
2022-12-19 11:05:23 +0300

    add dayOfWeek by numbers

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 2. По заданному номеру дня недели вывести его название
+Console.Write("Введите номер дня недели: ");
+
+int a = int.Parse(Console.ReadLine()!);
+// int b = 2;
+//int c = 3;
+//int d = 4;
+//int e = 5;
+//int f = 6;
+//int g = 7;
+
+if (a == 1)
+{     Console.WriteLine("понедельник");}
+if (a == 2)
+{     Console.WriteLine("вторник");}
+if (a == 3)
+{     Console.WriteLine("среда");}
+if (a == 4)
+{     Console.WriteLine("четверг");}
+if (a == 5)
+{     Console.WriteLine("пятница");}
+if (a == 6)
+{     Console.WriteLine("суббота");}
+if (a == 7)
+{     Console.WriteLine("воскресенье");}
+
