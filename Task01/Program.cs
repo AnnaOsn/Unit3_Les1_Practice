e70892c1f71bbd2b3e444ea878e6ad030e933378
Anna_Osn <Anya_kas@live.ru>
2022-12-19 10:27:39 +0300

    add Task01

---
+++ Task01/Program.cs
@@ -0,0 +1,20 @@
+﻿// По двум заданным числам проверять является ли первое квадратом второго
+
+Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a == b*b)
+{     Console.WriteLine("yes");}
+    
+//Console.WriteLine(a);
+//Console.Write("min: ");
+//Console.WriteLine(b);
+
+else
+{
+    Console.WriteLine("no");
+}
+//Console.WriteLine(b);
+//Console.Write("min: ");
+//Console.WriteLine(a);
