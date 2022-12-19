f97abc107b3da990d1e841bd21618df4e64165d9
Anna_Osn <Anya_kas@live.ru>
2022-12-19 11:28:52 +0300

    add maxOfThree

---
+++ Task03/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Enter first number: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter second number: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Enter third number: ");
+int c = int.Parse(Console.ReadLine()!);
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+{     Console.Write("max = ");
+Console.WriteLine(max);}
